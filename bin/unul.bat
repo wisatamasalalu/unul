6d5ebb4b1e306f5079bc7265de477e99a7f4be68
Wisata Masa Lalu <wisatamasalalu@proton.me>
2026-03-30 21:42:36 +0700

    fix bug on biin/unul for executing program with .anu

--- bin/unul.bat
+++ bin/unul.bat
@@ -1,3 +1,6 @@
+import os
+            path_absolut = os.path.abspath(file_path)
+            interpreter.muat_anu(path_absolut)
 @echo off
 REM Eksekutor UNUL CLI untuk lingkungan Windows
 python "%~dp0unul" %*
